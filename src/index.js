29c2f788a72efd640ad85b5968e14bb7654fba4d
Binod Lama <binod.lama@es.ey.com>
2019-03-03 01:18:08 +0100

    Argument works

--- src/index.js
+++ src/index.js
@@ -3,31 +3,66 @@ import { GraphQLServer } from 'graphql-yoga'
 //Type definitions(schema -data structures)
 const typeDefs= `
      type Query {
-                id: ID!,
-                username:String!,
-                password:String!,
-                rol:String!,
-                puntos:Int!
+         greeting(name:String):String
+         add(a:Float!,b:Float):Float!
+               me:Persona!
+               post:Post!
                 }
+
+
+     type Persona{
+        username:String!,
+        password:String!,
+        rol:String!,
+        puntos:Int!
+     }  
+     type Post{
+        Id:ID!,
+        title:String!,
+        body:String!,
+        published:Boolean!
+     }            
+
 `
+
+
 
 //resolvers(setr of functions)
 const resolvers={
-    Query:{
+    Query:{       
+        me()
+        {
+            return {
+                username:'Binod',
+                password:'BBB',
+                rol:'Student',
+                puntos:10
+
+            } 
+        },
+        post()
+        {
+            return {
+                Id:'aaa',
+        title:'Graphql',
+        body:'Building first api',
+        published:false
+            } 
+
+        },
+
+      greeting(parent,args)
+      {
+        console.log(args)
+        if (args.name)
+        { return "HELLO: "+args.name}
+        else
+        {return "HELLO AAAAA !"} 
         
-       
-        username(){
-            return 'Binod'
-        },
-        password(){
-            return 'aaa'
-        },
-        rol(){
-            return 'Student'
-        },
-        puntos(){
-            return 21
-        }
+      },
+      add(parent, args){
+          return args.a + args.b
+      }
 
     }
 }
